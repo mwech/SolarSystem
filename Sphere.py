ebdb84fb783e34169f9909c003101fea854cd78b
mwech <mwech@student.tgm.ac.at>
2015-02-27 21:54:24 +0100

    Sphere Rotation now possible

--- Sphere.py
+++ Sphere.py
@@ -9,16 +9,17 @@ import sys
 def depth():
     glShadeModel(GL_SMOOTH)
     glEnable(GL_CULL_FACE)
+    glDepthFunc(GL_LESS)
     glEnable(GL_DEPTH_TEST)
 
 def light():
-    glEnable(GL_LIGHTING)
     lightZeroPosition = [10., 4., 10., 1.]
     lightZeroColor = [0.8, 1.0, 0.8, 1.0]  # green tinged
     glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
     glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
     glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+    glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
 
 def main():
@@ -26,17 +27,16 @@ def main():
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
     glutInitWindowSize(800, 800)
     glutCreateWindow(b"Scene")
-
-    depth()
-    light()
-
     glutDisplayFunc(display)
-
+    glutIdleFunc(display)
     glutMainLoop()
     return
 
+def idle( ):
+	glutPostRedisplay()
+
 def sphereMaterial():
-    color = [1.0, 0., 0., 1.]
+    color = [1.0, 1., 0.0, 1.]
     glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
 
 def drawSphere():
@@ -44,39 +44,49 @@ def drawSphere():
     glPushMatrix()
     try:
         glTranslatef(*position)
+        glRotatef(260, 1,0 , 0)
         glutSolidSphere(2, 40, 40)
     finally:
         glPopMatrix()
 
 def perspective():
     glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
     gluPerspective(40., 1., 1., 40.)
     glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
     gluLookAt(0, 0, 10,
               0, 0, 0,
               0, 1, 0)
     #glPushMatrix()
 
-def rotation():
-    starttime = time.time()
-    angle = (((time.time()-starttime)%10)/10)* 360
+starttime = time.time()
+
+def rotation( period = 10):
+    """Do rotation of the scene at given rate"""
+    #print(starttime)
+    angle = (((time.time()-starttime)%period)/period)* 360
+    print(angle)
     glRotate( angle, 0,1,0)
     return angle
 
-def display():
 
-    glClearColor(0., 0., 0., 1.)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    glPushMatrix()
+def display(clear = 1, swap=1):
+    if clear:
+        glClearColor(0., 0., 0., 1.)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    perspective()
+    light()
+    depth()
+    #glPushMatrix()
     sphereMaterial()
     rotation()
     drawSphere()
-    glPopMatrix()
+    #glPopMatrix()
+    if swap:
+        glutSwapBuffers()
 
-    glutSwapBuffers()
 
-    perspective()
     return
 
 if __name__ == '__main__': main()
