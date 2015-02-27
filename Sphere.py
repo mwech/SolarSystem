200175e49e3c97d46458614569faa3d0378161d9
mwech <mwech@student.tgm.ac.at>
2015-02-27 19:08:48 +0100

    Added some rotation examples

--- Sphere.py
+++ Sphere.py
@@ -1,22 +1,25 @@
+import time
+
 __author__ = 'mwech'
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
 import sys
 
+
 def main():
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(800,800)
+    glutInitWindowSize(800, 800)
     glutCreateWindow(b"Hallo")
 
-    glClearColor(0.,0.,0.,1.)
+    glClearColor(0., 0., 0., 1.)
     glShadeModel(GL_SMOOTH)
     glEnable(GL_CULL_FACE)
     glEnable(GL_DEPTH_TEST)
     glEnable(GL_LIGHTING)
-    lightZeroPosition = [10.,4.,10.,1.]
-    lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
+    lightZeroPosition = [10., 4., 10., 1.]
+    lightZeroColor = [0.8, 1.0, 0.8, 1.0]  # green tinged
     glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
     glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
@@ -24,21 +27,22 @@ def main():
     glEnable(GL_LIGHT0)
     glutDisplayFunc(display)
     glMatrixMode(GL_PROJECTION)
-    gluPerspective(40.,1.,1.,40.)
+    gluPerspective(40., 1., 1., 40.)
     glMatrixMode(GL_MODELVIEW)
-    gluLookAt(0,0,10,
-              0,0,0,
-              0,1,0)
+    gluLookAt(0, 0, 10,
+              0, 0, 0,
+              0, 1, 0)
     glPushMatrix()
     glutMainLoop()
     return
 
+
 def display():
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glPushMatrix()
-    color = [1.0,0.,0.,1.]
-    glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
-    glutSolidSphere(3,20,20)
+    color = [1.0, 0., 0., 1.]
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
+    glutSolidSphere(3, 20, 20)
     glPopMatrix()
     glutSwapBuffers()
     return
