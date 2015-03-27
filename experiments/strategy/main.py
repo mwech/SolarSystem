209daede69ce8f83a4d04acbc6ba9e233cca5baf
mwech <mwech@student.tgm.ac.at>
2015-03-27 22:52:13 +0100

    Strategy pattern without rotation and events implemented

--- experiments/strategy/main.py
+++ experiments/strategy/main.py
@@ -41,7 +41,7 @@ class main():
                 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
                 concrete.perform_design()
 
-                pygame.display.flip()
+                pygame.display.flip()##
                 pygame.time.wait(10)
 
             """
