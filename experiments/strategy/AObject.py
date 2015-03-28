62f7ea6f3485a57aa618defaf90d2ff94973a962
mwech <mwech@student.tgm.ac.at>
2015-03-28 18:02:56 +0100

    Adjusted size of objects

--- experiments/strategy/AObject.py
+++ experiments/strategy/AObject.py
@@ -45,10 +45,11 @@ class AObject(object):
 
         :return: /
         """
-        self._design_verhalten.drawSphere(0,0,0,1,1,"Sonne",1,number,0)
+        self._design_verhalten.drawSphere(0,0,0,1,2,"Sonne",1,number,0)
 
-        self._design_verhalten.drawSphere(-5,0,-2,2,0.5,"Planet",2,number, speedPlanet)
-        self._design_verhalten.drawSphere(-2,0,-2,3,0.2,"Mond",3,number, speedMond)
+        self._design_verhalten.drawSphere(-6,0,-2,2,0.6,"Planet",2,number, speedPlanet)
+        self._design_verhalten.drawSphere(-3,0,-2,3,0.3,"Mond",3,number, speedMond)
 
-        self._design_verhalten.drawSphere(4,0,-2,2,0.5,"Planet",2,number, speedPlanet)
-        self._design_verhalten.drawSphere(1,0,-1,3, 0.2,"Mond",3,number, speedMond)
+
+        self._design_verhalten.drawSphere(5,0,-2,2,0.6,"Planet",2,number, speedPlanet)
+        self._design_verhalten.drawSphere(1,0,-1,3, 0.3,"Mond",3,number, speedMond)
