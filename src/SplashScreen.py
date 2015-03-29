615be993fa7e5a60d5c419d14fe73f420ee61517
mwech <mwech@student.tgm.ac.at>
2015-03-29 11:53:09 +0200

    Adjusted SplashScreen Size

--- src/SplashScreen.py
+++ src/SplashScreen.py
@@ -5,7 +5,7 @@ from pygame.constants import DOUBLEBUF, OPENGL
 class SplashScreen():
     def initSplashScreen(self):
         """SPLASHSCREEN INIT """
-        screen = pygame.display.set_mode([1280, 720])  # Setting the size of the screen
+        screen = pygame.display.set_mode([800,600])  # Setting the size of the screen
         image = pygame.image.load("texturen/Splashscreen_v1.jpg").convert()  # Splashscreen
         logoimage = screen.blit(image, (0, 0))
         pygame.display.flip()
