0b0ee61d30ac54653cbbea80f7bf9e16169185f3
mwech <mwech@student.tgm.ac.at>
2015-04-18 15:13:57 +0200

    Shifted all unittests into the module UnitTestcases

--- tests/UnitTests.py
+++ tests/UnitTests.py
@@ -1,6 +1,6 @@
-
 import unittest
 from src.Design import Design
+from src.Util import Util
 
 """
 @author: Arif Balkan, Maximilian Wech
@@ -8,11 +8,12 @@ from src.Design import Design
 @description: Testen der Design Klasse
 """
 
-class Design_Testcases(unittest.TestCase):
+class UnitTestcases(unittest.TestCase):
 
     def setUp(self):
-        global design
+        global design, ut
         design = Design()
+        ut = Util()
 
     @unittest.expectedFailure
     def test_drawSphere_radius_less_than_0(self):
@@ -125,6 +126,52 @@ class Design_Testcases(unittest.TestCase):
         number = -10
         speed = False
         self.assertRaises(TypeError, art, number, speed)
+
+    ################ Tests für Util #####################
+    """
+    def testValidFilename(self):
+       actual = ut.validTexture("../src/texturen/moon.jpg")
+       expected = True
+       self.assertEquals(actual, expected)
+
+    def testValidFilename2(self):
+       actual = ut.validTexture("../src/texturen/world2.png")
+       expected = True
+       self.assertEquals(actual, expected)
+    """
+    def testValidFilename3(self):
+       actual = ut.validTexture("../src/texturen/world2.gif")
+       expected = False
+       self.assertEquals(actual, expected)
+
+    def testValidFilename4(self):
+       actual = ut.validTexture("../src/Splashscreen_v1.jpg")
+       expected = False
+       self.assertEquals(actual, expected)
+
+    def testValidFilename5(self):
+       actual = ut.validTexture(0)
+       expected = False
+       self.assertEquals(actual, expected)
+
+    def testValidFilename6(self):
+       actual = ut.validTexture(0.58)
+       expected = False
+       self.assertEquals(actual, expected)
+
+    def testValidFilename7(self):
+       actual = ut.validTexture('world2')
+       expected = False
+       self.assertEquals(actual, expected)
+
+    def testValidFilename8(self):
+       actual = ut.validTexture(-5)
+       expected = False
+       self.assertEquals(actual, expected)
+
+    def tearDown(self):
+        ut = None
+        deisgn = None
 
 if __name__ == '__main__':
     unittest.main()
